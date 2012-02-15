d3fd3aa57c45f3b4ed02403360eeb41735450a65
Paulo Mendoza & Chiqui Mercene <pair+paulo+chiqui@friendster.com>
2012-02-15 17:04:26 +0800

    Created Exceptions for Friendster Client API

---
+++ src/main/java/com/friendster/api/client/throwable/FriendsterAPIError.java
@@ -0,0 +1,16 @@
+package com.friendster.api.client.throwable;
+
+/**
+ * Friendster API Error
+ * Friendster Inc.
+ * 
+ * @author Paulo Mendoza & Chiqui Mercene
+ * @since Feb 15, 2012
+ *
+ */
+
+public class FriendsterAPIError extends Error {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/friendster/api/client/throwable/FriendsterAPIException.java
@@ -0,0 +1,22 @@
+package com.friendster.api.client.throwable;
+
+/**
+ * Friendster API Exception
+ * Friendster Inc.
+ * 
+ * @author Paulo Mendoza & Chiqui Mercene
+ * @since Feb 15, 2012
+ *
+ */
+public class FriendsterAPIException extends RuntimeException {
+	private static final long serialVersionUID = -648052697036883255L;
+	private Exception originalException;
+	
+	public FriendsterAPIException(Exception e) {
+		this.originalException = e;
+	}
+
+	public Exception getOriginalException() {
+		return this.originalException;
+	}
+}
