84dbdd0d0b4860eee863ac3ffded3bd8fd3d91b6
Chiqui Mercene, Francis Xavier Antazo & Paulo Mendoza <pair+chiqui+francis+pmendoza@friendster.com>
2012-05-07 15:11:30 +0800

    WIP Updated FriendsResponse

--- client-core/src/main/java/com/friendster/api/beans/FriendsResponse.java
+++ client-core/src/main/java/com/friendster/api/beans/FriendsResponse.java
@@ -2,7 +2,6 @@ package com.friendster.api.beans;
 
 import java.util.List;
 
-import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Namespace;
 import org.simpleframework.xml.Root;
@@ -11,7 +10,7 @@ import org.simpleframework.xml.Root;
 @Namespace(reference = "http://api.friendster.com/v1/")
 public class FriendsResponse {
 
-	@ElementList(entry = "friends")
+	@ElementList(name = "friends")
 	@Namespace(reference = "http://api.friendster.com/v1/")
 	//private List<UserFriends> friends;
 	private List<String> uid;
@@ -23,15 +22,4 @@ public class FriendsResponse {
 	public void setUid(List<String> uid) {
 		this.uid = uid;
 	}
-
-	
-
-//	public List<UserFriends> getFriends() {
-//		return friends;
-//	}
-//
-//	public void setFriends(List<UserFriends> friends) {
-//		this.friends = friends;
-//	}
-
 }
